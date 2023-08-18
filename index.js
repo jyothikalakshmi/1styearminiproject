7c00d35cac90ab1e457dbfebcc8ab0f74cfd0904
Jyothika Lakshmi M <jyothikalakshmim07@gmail.com>
2023-08-18 23:39:25 +0530

    Updated bookings tab

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.getElementById("defaultTab").click();
 
-function openTab(id){
-     // Declare all variables
+function openTab(id) {
+  // Declare all variables
   var i, tabcontent;
- 
+
   // Get all elements with class="tabContentDetails" and hide them
   tabcontent = document.getElementsByClassName("tabContentDetails");
   for (i = 0; i < tabcontent.length; i++) {
@@ -11,7 +11,7 @@ function openTab(id){
   }
 
   document.getElementById(id).style.display = "block";
- 
+
 }
 function myFunction() {
   // Get the SUBMIT DIV
@@ -21,41 +21,63 @@ function myFunction() {
   x.className = "show";
 
   // After 3 seconds, remove the show class from DIV
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 const wetCheckbox = document.getElementById('1');
 const dryCheckbox = document.getElementById('2');
 const bothCheckbox = document.getElementById('3');
 const wetWeightDiv = document.getElementById('wetWeight');
 const dryWeightDiv = document.getElementById('dryWeight');
+const totalCostPriceDiv = document.getElementById('totalCost');
+const totalCostDiv = document.getElementById('totalCostDiv');
+const dryWasteKg = document.getElementById('dryWasteKg');
+const wetWasteKg = document.getElementById('wetWasteKg');
+let totalCost=0;
+let dryTotalCost=0;
+let wetTotalCost=0;
+
 wetWeightDiv.style.display = 'none';
 dryWeightDiv.style.display = 'none';
+totalCostDiv.style.display = 'none';
+
 
 wetCheckbox.addEventListener('change', updateBothCheckbox);
 dryCheckbox.addEventListener('change', updateBothCheckbox);
 bothCheckbox.addEventListener('change', updateIndividualCheckboxes);
+
+wetWasteKg.addEventListener('change', function () {
+  updateTotalCost(wetWasteKg.value,'WET');
+});
+
+dryWasteKg.addEventListener('change', function () {
+  updateTotalCost(dryWasteKg.value,'DRY');
+});
 
 function updateBothCheckbox() {
   if (wetCheckbox.checked && dryCheckbox.checked) {
     bothCheckbox.checked = true;
     wetWeightDiv.style.display = 'block';
     dryWeightDiv.style.display = 'block';
+    totalCostDiv.style.display = 'flex';
   } else {
     bothCheckbox.checked = false;
-    if(wetCheckbox.checked){
+    if (wetCheckbox.checked) {
       wetWeightDiv.style.display = 'block';
       dryWeightDiv.style.display = 'none';
+      totalCostDiv.style.display = 'flex';
     }
-    else if(dryCheckbox.checked){
+    else if (dryCheckbox.checked) {
       wetWeightDiv.style.display = 'none';
       dryWeightDiv.style.display = 'block';
+      totalCostDiv.style.display = 'flex';
     }
-    else{
+    else {
       wetWeightDiv.style.display = 'none';
       dryWeightDiv.style.display = 'none';
+      totalCostDiv.style.display = 'none';
     }
   }
-  
+
 }
 
 function updateIndividualCheckboxes() {
@@ -64,10 +86,63 @@ function updateIndividualCheckboxes() {
     dryCheckbox.checked = true;
     wetWeightDiv.style.display = 'block';
     dryWeightDiv.style.display = 'block';
+    totalCostDiv.style.display = 'flex';
   } else {
     wetCheckbox.checked = false;
     dryCheckbox.checked = false;
     wetWeightDiv.style.display = 'none';
     dryWeightDiv.style.display = 'none';
+    totalCostDiv.style.display = 'none';
+  }
+}
+function updateTotalCost(value,wasteType) {
+
+  if(wasteType=='DRY'){
+    if(value>0 && value<=2){
+      dryTotalCost=30;
+    }
+    else if(value>2 && value<=4){
+      dryTotalCost=50;
+    }
+    else{
+      let extraValue=value-4;
+      dryTotalCost=50+(extraValue*10);
+    }
+    if(value==0){
+      dryTotalCost=0;
+    }
+  }
+  else if(wasteType=='WET'){
+    if(value>0&&value<=1){
+      wetTotalCost=25;
+    }
+    else if(value>1&&value<=2){
+      wetTotalCost=35;
+    }
+    else if(value>2&&value<=3){
+      wetTotalCost=45;
+    }
+    else if(value>3&&value<=4){
+      wetTotalCost=55;
+    }
+    else{
+      let extraValue=value-4;
+      wetTotalCost=55+(extraValue*15);
+    }
+    if(value==0){
+      wetTotalCost=0;
+    }
+    
+  }
+  totalCost=wetTotalCost+dryTotalCost;
+  totalCostPriceDiv.innerHTML=totalCost+' Rupees';
+  
+}
+function validateTime(selectedTime) {
+  const selectedHour = parseInt(selectedTime.split(':')[0]);
+  
+  if (selectedHour < 6 || selectedHour > 20) {
+      alert('Please select a time between 6 AM and 8 PM.');
+      document.getElementById('timeInput').value = ''; // Reset the input value
   }
 }
